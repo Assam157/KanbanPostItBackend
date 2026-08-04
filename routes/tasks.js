d689819e4a3b478b558368bd09dc1e6269dfa98d
Maitreya Gupta 1 <guptamaitreya0@gmail.com>
2026-08-04 11:04:00 +0530

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const Task = require('../models/Task');
-const User = require('../models/User');
-const auth = require('../middleware/auth');
+const Task = require('./models/Task');
+const User = require('./models/User');
+const auth = require('./middleware/auth');
 
 const router = express.Router();
 
